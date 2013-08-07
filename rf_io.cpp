a65fac43748f2c7798c2998251de34eebc1c0a24
unknown <royi@example.com>
2013-08-07 21:26:06 +0300

    unified rf multi packet protocol for arduino

--- rf_io.cpp
+++ rf_io.cpp
@@ -46,22 +46,31 @@ bool handle_read(RF24 *radio, uint8_t* buf, unsigned int len, uint8_t* data, uns
     return false;
 }
 
-bool handle_write(RF24 *radio, void* buf, unsigned int len, int repeat){
-    uint8_t tx_buf[32];
-    if(len<radio->getPayloadSize()) {
-        tx_buf[0] = (stand_alone << 6) | (len+1);
-        memcpy(tx_buf+1, buf, len);
-        //printf("sending message, length: %d content:\n", len);
-        for(int i=0;i<len+1;i++) {
-            //printf("0x%X ", tx_buf[i]);
-        }
-        //printf("\n");
-        radio->stopListening();
+bool _send(RF24 *radio, void* buf, unsigned int len, int repeat)
+{
 		int i = 0;
 		bool success = false;
+        /*printf("sending message, length: %d content:\n", len);
+        for(int i=0;i<len+1;i++) {
+            printf("0x%X ", buf);
+        }
+        printf("\n");*/
 		while(!success && (i++ < repeat)) {
-			success = radio->write(tx_buf, len+1);
+			success = radio->write(buf, len);
 		}
+		return success;
+}
+
+bool handle_write(RF24 *radio, void* buf, unsigned int len, int repeat){
+    uint8_t tx_buf[32];
+	uint8_t* cur_ptr = NULL;
+	uint8_t ps = radio->getPayloadSize();
+	bool success = false;
+    if(len<ps) {
+        tx_buf[0] = (stand_alone << 6) | (len+1);
+        memcpy(tx_buf+1, buf, len);
+        radio->stopListening();
+		success = _send(radio, tx_buf, len+1, repeat);
         radio->startListening();
         if(success) {
             Serial.println("send single packet succeded");
@@ -70,8 +79,45 @@ bool handle_write(RF24 *radio, void* buf, unsigned int len, int repeat){
 		Serial.println("send single packet failed");
         return false;
     }
-	Serial.println("packet too big");
+	else {
+		uin8_t remain_size = len;
+        tx_buf[0] = first_packet << 6;
+        tx_buf[0] |= ps;
+		*((uint16_t*)(tx_buf+1)) = len;
+		memcpy(tx_buf+3, buf, ps-3);
+        radio->stopListening();
+		success = _send(radio, tx_buf, len+1, repeat);
+        if(!success) {
+			Serial.println("first packet failed");
+			radio->startListening();
+			return false;
+		}
+		remain_size -= ps-3;
+		cur_ptr = buf + ps - 3;
+		while(remain_size > ps-1 && success) {
+			tx_buf[0] = continued_packet << 6;
+			tx_buf[0] |= ps;
+			memcpy(tx_buf+1, cur_ptr, ps-1);
+			cur_ptr += ps-1;
+			remain_size -= ps-1;
+			success = _send(radio, tx_buf, ps, repeat);
+		}	
+        if(!success) {
+			Serial.println("continued packet failed");
+			radio->startListening();
+			return false;
+		}
+        tx_buf[0] = (last_packet << 6) | (remain_size+1);
+        memcpy(tx_buf+1, cur_ptr, remain_size);
+		success = _send(radio, tx_buf, remain_size+1, repeat);
+        if(!success) {
+			Serial.println("last packet failed");
+		}
+		else {
+		    Serial.println("send multi packet succeded");
+		}
+		radio->startListening();
+		return success;
+	}
     return false;
 }
-
-
